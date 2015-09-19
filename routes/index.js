4dde260ff2f9d59db6d0a990c28199c836f53589
qiushi <qiushi@qiushideMacBook-Pro.local>
2015-09-19 12:27:12 +0800

    testHtml

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/heartqOl',function(req,res,next){
+	res.render('heartq_test',{title:'HeartQol',data:[
+		{
+			title:'Walk indoors on level ground？',
+			answer:{
+				a:'No',
+				b:'A little',
+				c:'some',
+				d:'A Lot'
+			}	
+		},{
+			title:'Walk indoors on level ground？',
+			answer:{
+				a:'No',
+				b:'A little',
+				c:'some',
+				d:'A Lot'
+			}	
+		},{
+			title:'Walk indoors on level ground？',
+			answer:{
+				a:'No',
+				b:'A little',
+				c:'some',
+				d:'A Lot'
+			}	
+		},{
+			title:'Walk indoors on level ground？',
+			answer:{
+				a:'No',
+				b:'A little',
+				c:'some',
+				d:'A Lot'
+			}	
+		}]});
+})
+
+module.exports = router;
