545fa693c24cfe8eaeb5c83c5e784890e3f63cbc
qiushi <qiushi@qiushideMacBook-Pro.local>
2015-10-08 17:59:05 +0800

    update

--- app.js
+++ app.js
@@ -37,16 +37,16 @@ app.use('/wechat', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
   console.log(message);
-  if (message.FromUserName === 'diaosi') {
+  if (message.Content === 'diaosi') {
     // 回复屌丝(普通回复)
     res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
+  } else if (message.Content === 'text') {
     //你也可以这样回复text类型的信息
     res.reply({
       content: 'text object',
       type: 'text'
     });
-  } else if (message.FromUserName === 'hehe') {
+  } else if (message.Content === 'hehe') {
     // 回复一段音乐
     res.reply({
       type: "music",
