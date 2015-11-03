0b61ec0451da71307dfd0a3c8a00df8bdda161a1
qiushi <870736375@qq.com>
2015-11-03 21:07:55 +0800

    index

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -8,6 +8,7 @@ $(function(){
             var isDone = confirm('是否完成此项任务');
             if(isDone){
 
+
             }else{
 
             }
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var request = require('request');
 var config = require('../config');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('showPage', {title: 'Express'});
 });
 
 router.get('/heartqOl', function (req, res, next) {
