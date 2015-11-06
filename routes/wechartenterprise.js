a1b53592ada90124ba00c953a76b4d9d7dddb386
qiushi <870736375@qq.com>
2015-11-06 12:27:01 +0800

    getuser

--- routes/wechartenterprise.js
+++ routes/wechartenterprise.js
@@ -7,10 +7,23 @@ var enterprise_config = {
  	encodingAESKey: 'QqiPktpEjnX3pLV5LIAjBktq5bUVG1dJsKVw0C8H2nP',
     corpId: 'wx306a8629aca93739'
 }
+var API = require('wechat-enterprise').API;
 
-router.use(wechatEnterprise(enterprise_config, function(req, res, next){;
-	console.log(req);
+var CorpID = 'wx306a8629aca93739';
+var Secret = 'iIChKj2NbXS74RlDHpr2_9f3r6VUIKbYueydJhQj58BqfUMuPnPvodEzbx0tUTq6';
+// 构造实例
 
+
+
+router.use(wechatEnterprise(enterprise_config, function(req, res, next){
+	var message = req.weixin;
+	var userId = message.FromUserName;
+	console.log('userId ='+userId);
+	var AgentID  = message.AgentID;
+	var api = new API(CorpID, Secret, AgentID);
+	api.getUser(userId, function (err, data, res) {
+	console.log(data);
+	});
 
 }));
 
