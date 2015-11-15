15524606b6ce32a87f5fe349f9345f3c887adebc
qiushi <870736375@qq.com>
2015-11-15 15:51:30 +0800

    new server

--- config/index.js
+++ config/index.js
@@ -5,14 +5,13 @@ if(config === null){
 		config = {
 			APPID :'wxb4fb29266130bb85',
        		APPSECRET :'675f1cd7edfcaba17b987c44c83e0a6b',
-			server:"http://123.56.227.132:8080"
+			server:"http://10.251.1.46:8080"
 		}
-
 	}else if(env === 'testing'){
 		config = {
 			 APPID :'wxb4fb29266130bb85',
        		 APPSECRET :'675f1cd7edfcaba17b987c44c83e0a6b',
-			 server:"http://192.168.0.101:8081"
+			 server:"http://123.56.126.231:8080"
 		}
 	}else{
 		config = {
